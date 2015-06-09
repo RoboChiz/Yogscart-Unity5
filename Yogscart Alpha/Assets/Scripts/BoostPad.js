0980edca3996fd32b6c741930797cd988fe7ad3b
Steamfireentertainment <ross9093bus@gmail.com>
2015-06-09 21:21:33 +0100

    Update #16 - Added Characters

--- Yogscart Alpha/Assets/Scripts/BoostPad.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-
-function OnTriggerEnter (other : Collider) {
-
-if(other.transform.parent.parent.GetComponent(kartScript) != null){
-other.transform.parent.parent.GetComponent(kartScript).StartCoroutine("Boost");
-}
-
-}
