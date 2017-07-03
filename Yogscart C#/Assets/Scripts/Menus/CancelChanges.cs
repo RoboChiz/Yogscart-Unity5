2a361e07702536b9a3439a105b4772db87248542
RoboChiz <robochiz@gmail.com>
2017-07-03 16:27:35 +0100

    Yogscart C# Update 62 - Everything you are, gone in a moment; like breath on a mirror.

---
+++ Yogscart C#/Assets/Scripts/Menus/CancelChanges.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CancelChanges : GamePopup
+{
+
+    public override string text { get { return "Would you like to confirm changes?"; } }
+
+    protected override void DoInput()
+    {
+        if (!noSelected)
+        {
+            FindObjectOfType<Options>().SaveEverything();
+        }
+
+        FindObjectOfType<Options>().somethingChanged = false;
+        FindObjectOfType<Options>().Quit();
+        HidePopUp();
+    }
+    
+}
